1b848e69a2006f1e4c525f2882f584ce21e90878
Lennard Berger <berger@mecodia.de>
2017-10-25 14:47:13 +0200

    [api] Bypass CSRF to prevent 403

--- aggregator/urls.py
+++ aggregator/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    url(r'^$', GraphQLView.as_view(graphiql=True)),
+    url(r'^$', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
